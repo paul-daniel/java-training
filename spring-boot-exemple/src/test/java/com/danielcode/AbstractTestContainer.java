a88fbf5fd923f5bc6ab2ea03d0ffb6797b21df26
paul-daniel <dantchatchouang@gmail.com>
2023-09-26 20:00:46 +0200

    add unit tests

--- spring-boot-exemple/src/test/java/com/danielcode/AbstractTestContainer.java
+++ spring-boot-exemple/src/test/java/com/danielcode/AbstractTestContainer.java
@@ -1,12 +1,17 @@
 package com.danielcode;
 
+import com.github.javafaker.Faker;
 import org.flywaydb.core.Flyway;
 import org.junit.jupiter.api.BeforeAll;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+
+import javax.sql.DataSource;
 
 @Testcontainers
 public abstract class AbstractTestContainer {
@@ -45,4 +50,19 @@ public abstract class AbstractTestContainer {
                 postgreSQLContainer::getPassword
         );
     }
+
+    private static DataSource getDataSource(){
+        return DataSourceBuilder.create()
+                .driverClassName(postgreSQLContainer.getDriverClassName())
+                .url(postgreSQLContainer.getJdbcUrl())
+                .username(postgreSQLContainer.getUsername())
+                .password(postgreSQLContainer.getPassword())
+                .build();
+    }
+
+    protected static JdbcTemplate getJdbcTemplate(){
+        return new JdbcTemplate(getDataSource());
+    }
+
+    protected static Faker FAKER = new Faker();
 }
---
+++ spring-boot-exemple/src/test/java/com/danielcode/customer/CustomerJDBCDataAccessServiceTest.java
@@ -0,0 +1,247 @@
+package com.danielcode.customer;
+
+import com.danielcode.AbstractTestContainer;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CustomerJDBCDataAccessServiceTest extends AbstractTestContainer {
+
+    private CustomerJDBCDataAccessService underTest;
+    private final CustomerRowMapper customerRowMapper = new CustomerRowMapper();
+
+    @BeforeEach
+    void setup(){
+        underTest = new CustomerJDBCDataAccessService(
+                getJdbcTemplate(),
+                customerRowMapper
+        );
+    }
+
+    @Test
+    void selectAllCustomers() {
+        // GIVEN
+        Customer customer = new Customer(
+                FAKER.name().firstName(),
+                FAKER.internet().safeEmailAddress() + "-" + UUID.randomUUID(),
+                FAKER.number().numberBetween(16,99)
+        );
+        underTest.insertCustomer(customer);
+
+        // WHEN
+        List<Customer> customers = underTest.selectAllCustomers();
+
+        // THEN
+        assertThat(customers).isNotEmpty();
+    }
+
+    @Test
+    void selectCustomerById() {
+        // GIVEN
+        Customer customer = new Customer(
+                FAKER.name().firstName(),
+                FAKER.internet().safeEmailAddress() + "-" + UUID.randomUUID(),
+                FAKER.number().numberBetween(16,99)
+        );
+        underTest.insertCustomer(customer);
+
+        int id = underTest.selectAllCustomers()
+                .stream()
+                .filter(c -> c.getEmail().equals(customer.getEmail()))
+                .map(Customer::getId)
+                .findFirst()
+                .orElseThrow();
+
+        // WHEN
+        Optional<Customer> actual = underTest.selectCustomerById(id);
+
+        // THEN
+        assertThat(actual).isPresent().hasValueSatisfying(
+                c -> {
+                    assertThat(c.getId()).isEqualTo(id);
+                    assertThat(c.getEmail()).isEqualTo(customer.getEmail());
+                    assertThat(c.getName()).isEqualTo(customer.getName());
+                    assertThat(c.getAge()).isEqualTo(customer.getAge());
+                }
+        );
+    }
+
+    @Test
+    void willReturnEmptyWhenSelectCustomerByID() {
+        // GIVEN
+        int id = -1;
+
+        // WHEN
+        Optional<Customer> actual = underTest.selectCustomerById(id);
+
+        // THEN
+        assertThat(actual).isEmpty();
+    }
+
+    @Test
+    void insertCustomer() {
+        // GIVEN
+        Customer customer = new Customer(
+                FAKER.name().firstName(),
+                FAKER.internet().safeEmailAddress() + "-" + UUID.randomUUID(),
+                FAKER.number().numberBetween(16,99)
+        );
+
+        underTest.insertCustomer(customer);
+
+        Customer customerInserted = underTest.selectAllCustomers()
+                .stream()
+                .filter(c -> c.getEmail().equals(customer.getEmail()))
+                .findFirst()
+                .orElseThrow();
+
+        // WHEN
+
+
+        // THEN
+        assertThat(customerInserted.getEmail()).isEqualTo(customer.getEmail());
+        assertThat(customerInserted.getAge()).isEqualTo(customer.getAge());
+        assertThat(customerInserted.getName()).isEqualTo(customer.getName());
+    }
+
+    @Test
+    void deleteCustomerById() {
+        // GIVEN
+        Customer customer = new Customer(
+                FAKER.name().firstName(),
+                FAKER.internet().safeEmailAddress() + "-" + UUID.randomUUID(),
+                FAKER.number().numberBetween(16,99)
+        );
+        underTest.insertCustomer(customer);
+
+        int id = underTest.selectAllCustomers()
+                .stream()
+                .filter(c -> c.getEmail().equals(customer.getEmail()))
+                .map(Customer::getId)
+                .findFirst()
+                .orElseThrow();
+
+        // WHEN
+        underTest.deleteCustomerById(id);
+        Optional<Customer> actual = underTest.selectCustomerById(id);
+
+        // THEN
+        assertThat(actual).isEmpty();
+    }
+
+    @Test
+    void updateCustomerInfos() {
+        // GIVEN
+        Customer customer = new Customer(
+                FAKER.name().firstName(),
+                FAKER.internet().safeEmailAddress() + "-" + UUID.randomUUID(),
+                FAKER.number().numberBetween(16,99)
+        );
+        underTest.insertCustomer(customer);
+
+        int id = underTest.selectAllCustomers()
+                .stream()
+                .filter(c -> c.getEmail().equals(customer.getEmail()))
+                .map(Customer::getId)
+                .findFirst()
+                .orElseThrow();
+
+        Customer updatedCustomer = new Customer(
+                id,
+                "jean85" + "-" + UUID.randomUUID(),
+                "jean85@gmail.com" + "-" + UUID.randomUUID(),
+                20
+
+        );
+
+        // WHEN
+        underTest.updateCustomerInfos(updatedCustomer);
+        Optional<Customer> actual = underTest.selectCustomerById(id);
+
+        // THEN
+        assertThat(actual)
+                .isPresent()
+                .hasValueSatisfying(
+                        c -> {
+                            assertThat(c.getId()).isEqualTo(id);
+                            assertThat(c.getName()).isEqualTo(updatedCustomer.getName());
+                            assertThat(c.getEmail()).isEqualTo(updatedCustomer.getEmail());
+                            assertThat(c.getAge()).isEqualTo(updatedCustomer.getAge());
+                        }
+                );
+    }
+
+    @Test
+    void existsCustomerWithId() {
+        // GIVEN
+        Customer customer = new Customer(
+                FAKER.name().firstName(),
+                FAKER.internet().safeEmailAddress() + "-" + UUID.randomUUID(),
+                FAKER.number().numberBetween(16,99)
+        );
+        underTest.insertCustomer(customer);
+
+        int id = underTest.selectAllCustomers()
+                .stream()
+                .filter(c -> c.getEmail().equals(customer.getEmail()))
+                .map(Customer::getId)
+                .findFirst()
+                .orElseThrow();
+
+        // WHEN
+        boolean expected = underTest.existsCustomerWithId(id);
+
+        // THEN
+        assertThat(expected).isTrue();
+    }
+
+    @Test
+    void doesNotExistWithId(){
+        // WHEN
+        boolean expected = underTest.existsCustomerWithId(-1);
+
+        // THEN
+        assertThat(expected).isFalse();
+    }
+
+    @Test
+    void existsPersonWithEmail() {
+        // GIVEN
+        Customer customer = new Customer(
+                FAKER.name().firstName(),
+                FAKER.internet().safeEmailAddress() + "-" + UUID.randomUUID(),
+                FAKER.number().numberBetween(16,99)
+        );
+        underTest.insertCustomer(customer);
+
+        String email = underTest.selectAllCustomers()
+                .stream()
+                .map(Customer::getEmail)
+                .filter(cEmail -> cEmail.equals(customer.getEmail()))
+                .findFirst()
+                .orElseThrow();
+
+        // WHEN
+        boolean expected = underTest.existsPersonWithEmail(email);
+
+        // THEN
+        assertThat(expected).isTrue();
+    }
+
+    @Test
+    void doesNotExistPersonWithEmail(){
+        // WHEN
+        boolean expected = underTest.existsPersonWithEmail("test-1.%s@gmail.com".formatted(UUID.randomUUID()));
+
+        // THEN
+        assertThat(expected).isFalse();
+    }
+}

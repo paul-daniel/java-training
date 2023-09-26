a53c3b4b63061fd1f2c4df50582afc898f9c12bd
paul-daniel <dantchatchouang@gmail.com>
2023-09-26 15:07:34 +0200

    abstract Testcontaine configuration

---
+++ spring-boot-exemple/src/test/java/com/danielcode/TestcontainersTestContainer.java
@@ -0,0 +1,15 @@
+package com.danielcode;
+
+import org.junit.jupiter.api.Test;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Testcontainers
+public class TestcontainersTestContainer extends AbstractTestContainer {
+    @Test
+    void canStartPostgresDB() {
+        assertThat(postgreSQLContainer.isRunning()).isTrue();
+        assertThat(postgreSQLContainer.isCreated()).isTrue();
+    }
+}
